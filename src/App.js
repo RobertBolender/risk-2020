a3b0887541a17a16fbae8547812b4d8502db1b30
Robert Bolender <Robert.Bolender.3@gmail.com>
2019-12-23 21:46:12 -0800

    Add first terrible version of the game.
    The interface is the product.
    https://medium.com/@bobpritchett/the-interface-is-the-product-4e5392574388
    
    The interface of this project is a page that displays results from
    pseudo-random simulated games. The initial implementation of these games
    may be incredibly simplistic, but my goal is to leave each commit with a
    functioning interface.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { Random } from "./Random";
 
-export function App() {
-  return <h1>Hello World!</h1>;
+const SEED = 422111495; // https://xkcd.com/221/
+
+const PRNG = new Random(SEED);
+
+const coinFlip = () => PRNG.next() % 2 === 0;
+const headsOrTails = () => (coinFlip() ? "Heads" : "Tails");
+
+export const App = () => {
+  const results = Array.from({ length: 20 }).map(() => headsOrTails());
+  const winCountPlayerA = results.filter(x => x === "Heads").length;
+  const winCountPlayerB = results.filter(x => x === "Tails").length;
+  return (
+    <div>
+      <h1>R.I.S.K. 2020</h1>
+      <h2>Rules</h2>
+      <p>
+        Player A goes first. Player A flips a coin. If 'Heads', Player A wins.
+        If 'Tails', Player B wins.
+      </p>
+      <h2>Results</h2>
+      <p>Player A won: {formatTimes(winCountPlayerA)}</p>
+      <p>Player B won: {formatTimes(winCountPlayerB)}</p>
+      <ul>
+        {results.map((result, index) => (
+          <li key={index}>{result}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+function formatTimes(number) {
+  return number === 1 ? `${number} time` : `${number} times`;
 }
